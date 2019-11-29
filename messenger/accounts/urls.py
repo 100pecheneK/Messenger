63027e4051c270b25dbcb241ac83537022e3b704
100pecheneK <mistermihail23@gmail.com>
2019-11-29 09:32:45 +0500

    Сделал
    Добавил новый модуль
    django-progressive-web-app==0.1.1
    Добавил новое приложение
    Работает PWA
    Улучшен дизайн

--- messenger/accounts/urls.py
+++ messenger/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import \
     RegisterView, LoginView, logout_view, \
     main, settings, user_management, \
-    personal_settings, EditNamesView
+    personal_settings, EditNamesView, delete_user, contacts
 
 
 app_name = 'accounts'
@@ -10,10 +10,10 @@ urlpatterns = [
     path('', main, name='main'),
     path('settings/', settings, name='settings'),
     path('user_management/', user_management, name='user_management'),
-
+    path('contacts', contacts, name='contacts'),
     path('personal_settings/', personal_settings, name='personal_settings'),
     path('edit_user_names/', EditNamesView.as_view(), name='edit_user_names'),
-    # path('edit_user_login_view/', EditUserLoginView.as_view(), name='edit_user_login'),
+    path('delete_user/<int:user_id>', delete_user, name='delete_user'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
